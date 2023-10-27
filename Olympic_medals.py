a232164099028f7cf7ecb7774f9bacb851740045
priyarkamath <88969140+priyarkamath@users.noreply.github.com>
2023-10-27 09:49:59 +0530

    Final_v1

--- Olympic_medals.py
+++ Olympic_medals.py
@@ -15,7 +15,7 @@ teams.corr()["medals"]
 sns.lmplot(x="age", y ="medals", data=teams, fit_reg= True, ci=None)
 teams.hist(column="medals", bins = 25)
 teams[teams.isnull().any(axis =1)]
-teams.dropna()
+teams.dropna(how='any', inplace=True)
 
 teams.shape
 teams["year"].max()
@@ -23,7 +23,8 @@ teams["year"].max()
 #divide the data to training and testing 
 train_data = teams[teams["year"]<2012].copy()
 test_data = teams[teams["year"]>=2012].copy()
-
+#test_data.shape
+#train_data.shape
 predictor = ["athletes", "prev_medals"]
 target = "medals"
 from sklearn.linear_model import LinearRegression
@@ -31,3 +32,16 @@ reg = LinearRegression()
 
 
 reg.fit(train_data[predictor], train_data["medals"])
+predictions = reg.predict(test_data[predictor])
+test_data["prediction"] = predictions
+#test_data
+test_data.loc[test_data["prediction"]<0,"prediction"] = 0
+test_data["prediction"] = test_data["prediction"].round()
+
+
+#MEASURE hte ERROR
+from sklearn.metrics import mean_absolute_error
+error_linearReg = mean_absolute_error(test_data["medals"], test_data["prediction"])
+
+print(error_linearReg)
+
