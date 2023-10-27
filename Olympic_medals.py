2df922b8e793e314420dc6659aac33c2453295df
priyarkamath <88969140+priyarkamath@users.noreply.github.com>
2023-10-27 07:37:10 +0530

    day2_m

--- Olympic_medals.py
+++ Olympic_medals.py
@@ -9,24 +9,25 @@ import pandas as pd
 import seaborn as sns
 teams  = pd.read_csv("teams.csv")
 teams = teams[["team", "country","year", "athletes","age","prev_medals", "medals"]]
-teams
+teams.shape
 teams.corr()["medals"]
 
 sns.lmplot(x="age", y ="medals", data=teams, fit_reg= True, ci=None)
 teams.hist(column="medals", bins = 25)
-teams[teams.isnull().any(axis = 1)]
+teams[teams.isnull().any(axis =1)]
 teams.dropna()
-teams
+
+teams.shape
 teams["year"].max()
 
 #divide the data to training and testing 
 train_data = teams[teams["year"]<2012].copy()
 test_data = teams[teams["year"]>=2012].copy()
 
-
+predictor = ["athletes", "prev_medals"]
+target = "medals"
 from sklearn.linear_model import LinearRegression
 reg = LinearRegression()
 
-predictor = ["athletes", "prev_medals"]
-target = "medals"
+
 reg.fit(train_data[predictor], train_data["medals"])
